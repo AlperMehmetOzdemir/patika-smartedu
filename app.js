78a6ed8003118529b12162d754be1c59992902c0
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-10-18 21:02:55 +0300

    Lesson 02 - Switched html files to ejs. Split recurring code into partials. Made navigation bar to reflect current active page.

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require("express");
-const mongoose = require("mongoose")
-const ejs = require("ejs");
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 5000;
 
 // TEMPLATE ENGINE
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
-app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ROUTES
-app.get("/", (req,res) => {
-  res.send("<h1>Index page</h1>")
-})
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
 
-
-
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
+});
 
 // LISTEN
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-})
+});
