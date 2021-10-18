41eaf011030705a2f57ca8595a2acada38fbabe9
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-10-18 22:01:16 +0300

    Lesson 04 - Initial Course model and create course router & controller.

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
-const pageRoute = require("./routes/pageRoute")
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// CONNECT DB
+mongoose
+  .connect('mongodb://localhost/smartedu-test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connected succesfully');
+  });
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -18,6 +29,7 @@ app.use(express.json());
 
 // ROUTES
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 // LISTEN
 app.listen(PORT, () => {
